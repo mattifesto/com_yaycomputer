54350cb7b7be0940b98318af14f14393a99faa10
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-05-02 11:40:16 -0700

    Add YCPageFooterView

---
+++ classes/YCPageFooterView/YCPageFooterView.php
@@ -0,0 +1,34 @@
+<?php
+
+final class YCPageFooterView {
+
+    /**
+     * @return null
+     */
+    public static function renderModelAsHTML(stdClass $model) {
+        echo '<div class="YCPageFooterViewFill" style="flex: 1 1 auto;"></div>';
+
+        CBContainerView::renderModelAsHTML((object)[
+            'subviews' => [
+                (object)[
+                    'className' => 'CBThemedTextView',
+                    'center' => true,
+                    'contentAsHTML' => 'Copyright &copy; 2016 - ' . gmdate('Y') . ' ' . CBSiteNameHTML,
+                    'themeID' => YCModels::CBThemeIDForYCPageFooterViewCopyright,
+                ],
+            ],
+            'themeID' => YCModels::CBThemeIDForYCPageFooterViewContainer,
+        ]);
+    }
+
+    /**
+     * @param stdClass $spec
+     *
+     * @return stdClass
+     */
+    public static function specToModel(stdClass $spec) {
+        return (object)[
+            'className' => __CLASS__,
+        ];
+    }
+}
