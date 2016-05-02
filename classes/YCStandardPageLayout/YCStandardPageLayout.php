a283198da4cfec21a7feeebb56c9e3d3ce19da89
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-05-02 11:53:03 -0700

    Add YCStandardPageLayout

---
+++ classes/YCStandardPageLayout/YCStandardPageLayout.php
@@ -0,0 +1,33 @@
+<?php
+
+final class YCStandardPageLayout {
+
+    /**
+     * @param stdClass $layoutModel
+     * @param callable $renderContentCallback
+     *
+     * @return null
+     */
+    public static function render(stdClass $layoutModel, callable $renderContentCallback) {
+        YCPageHeaderView::renderModelAsHTML((object)[]);
+
+        echo '<main class="YCStandardPageLayout">';
+
+        $renderContentCallback();
+
+        echo '</main>';
+
+        YCPageFooterView::renderModelAsHTML((object)[]);
+    }
+
+    /**
+     * @param stdClass $spec
+     *
+     * @return stdClass
+     */
+    public static function specToModel(stdClass $spec) {
+        return (object)[
+            'className' => __CLASS__,
+        ];
+    }
+}
