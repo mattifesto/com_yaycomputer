395fe7cc26c1f26f6df9dc11e8b80651932d8eb8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-05-09 19:38:05 -0700

    Add YCAppleProduct

---
+++ classes/YCAppleProduct/YCAppleProduct.php
@@ -0,0 +1,37 @@
+<?php
+
+final class YCAppleProduct {
+
+    /**
+     * @param model $spec
+     *
+     * @return ?model
+     */
+    static function CBModel_build(stdClass $spec): ?stdClass {
+        return (object)[
+            'content' => CBModel::valueToString($spec, 'content'),
+            'productURL' => trim(CBModel::valueToString($spec, 'productURL')),
+            'sort' => CBModel::valueAsInt($spec, 'sort'),
+        ];
+    }
+
+    /**
+     * @param model $spec
+     *
+     * @return ?hex160
+     */
+    static function CBModel_toID(stdClass $spec): ?string {
+        $moniker = trim(CBModel::valueToString($spec, 'moniker'));
+
+        return sha1("YCAppleProduct {$moniker}");
+    }
+
+    /**
+     * @param [model] $models
+     *
+     * @return void
+     */
+    static function CBModels_willSave(array $models): void {
+        YCApplePageUpdateTask::restart();
+    }
+}
