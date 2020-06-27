4da3abf12a204b0fee65da0e1d7d19c58148834d
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-06-27 14:42:46 -0700

    Remove handlers/handle,blog.php

--- handlers/handle,blog.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Earlier this site had a specific /blog/ URI that has since been moved to the
- * front page. We redirect here instead of customizing the .htaccess file.
- */
-
-header('Location: /', true, 301);
