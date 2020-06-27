3d03bdaa972cac830ed60b5ed539dcfe3e79fa9e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-06-27 14:43:38 -0700

    Garden YCMainMenu.php

--- classes/YCMainMenu/YCMainMenu.php
+++ classes/YCMainMenu/YCMainMenu.php
@@ -2,40 +2,45 @@
 
 final class YCMainMenu {
 
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
+
+
     /**
      * @return void
      */
     static function CBInstall_install(): void {
-        $originalSpec = CBModels::fetchSpecByID(YCMainMenu::ID());
-
-        if (empty($originalSpec)) {
-            $spec = (object)[
+        $updater = CBModelUpdater::fetch(
+            (object)[
+                'className' => 'CBMenu',
                 'ID' => YCMainMenu::ID(),
-                'title' => 'Yay! Computer!',
+                'title' => 'Yay Computer',
                 'titleURI' => '/',
-                'items' => [],
-            ];
-        } else {
-            $spec = CBModel::clone($originalSpec);
-        }
+            ]
+        );
 
-        $spec->className = 'CBMenu';
-
-        /* save if modified */
-
-        if ($spec != $originalSpec) {
-            CBDB::transaction(function () use ($spec) {
-                CBModels::save($spec);
-            });
-        }
+        CBModelUpdater::save($updater);
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
     static function CBInstall_requiredClassNames(): array {
-        return ['CBModels'];
+        return [
+            'CBMenu',
+            'CBModelUpdater',
+        ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
+
+    /* -- functions -- -- -- -- -- */
+
+
 
     /**
      * @return ID
@@ -43,4 +48,5 @@ final class YCMainMenu {
     static function ID(): string {
         return 'b1ebf1fcd2bea9bd084c24e3174b99866f4c7ea8';
     }
+
 }
