b547a356a37f70c56f711ded9f3b919b4424f3b6
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-09-17 16:08:23 -0700

    Unpublish the Apple page and remove it from the main menu in YCApplePage.php

--- classes/YCApplePage/YCApplePage.php
+++ classes/YCApplePage/YCApplePage.php
@@ -5,99 +5,79 @@ final class YCApplePage {
     /**
      * @return void
      */
-    static function CBInstall_install(): void {
+    static function CBInstall_install(
+    ): void {
 
         /* page */
 
-        $originalPageSpec = CBModels::fetchSpecByID(YCApplePage::ID());
-
-        if (empty($originalPageSpec)) {
-            $pageSpec = (object)[
+        CBModelUpdater::update(
+            (object)[
                 'ID' => YCApplePage::ID(),
-            ];
-        } else {
-            $pageSpec = CBModel::clone($originalPageSpec);
-        }
-
-        CBModel::merge($pageSpec, (object)[
-            'className' => 'CBViewPage',
-            'classNameForSettings' => 'YCPageSettingsForResponsivePages',
-            'frameClassName' => 'YCPageFrame',
-            'isPublished' => true,
-            'title' => 'Apple',
-            'URI' => 'apple',
-        ]);
-
-        if (empty($pageSpec->publicationTimeStamp)) {
-            $pageSpec->publicationTimeStamp = time();
-        }
-
-        /* title view */
-
-        $viewSpec = CBView::findSubview($pageSpec, 'className', 'CBPageTitleAndDescriptionView');
-
-        if (empty($viewSpec)) {
-            $subviews = CBView::getSubviews($pageSpec);
-            $viewSpec = (object)[
-                'className' => 'CBPageTitleAndDescriptionView',
-            ];
-
-            array_unshift($subviews, $viewSpec);
-
-            CBView::setSubviews($pageSpec, $subviews);
-        }
-
-        if ($pageSpec != $originalPageSpec) {
-            CBDB::transaction(function () use ($pageSpec) {
-                CBModels::save($pageSpec);
-            });
-        }
+                'isPublished' => false,
+            ]
+        );
 
         /* main menu item */
 
-        $originalMenuSpec = CBModels::fetchSpecByID(YCMainMenu::ID());
-        $menuSpec = CBModel::clone($originalMenuSpec);
-        $items = CBModel::valueToArray($menuSpec, 'items');
-        $appleItem = null;
+        $updater = CBModelUpdater::fetch(
+            (object)[
+                'ID' => YCMainMenu::ID(),
+            ]
+        );
 
-        foreach ($items as $item) {
-            $name = CBModel::valueToString($item, 'name');
 
-            if ($name === 'apple') {
-                $appleItem = $item;
-                break;
+        $menuItems = CBModel::valueToArray(
+            $updater->working,
+            'items'
+        );
+
+        $updater->working->items = array_filter(
+            $menuItems,
+            function ($currentMenuItem) {
+                $currentMenuItemName = CBModel::valueToString(
+                    $currentMenuItem,
+                    'name'
+                );
+
+                if ($currentMenuItemName === 'apple') {
+                    return false;
+                } else {
+                    return true;
+                }
             }
-        }
+        );
 
-        if (empty($appleItem)) {
-            $items[] = (object)[
-                'className' => 'CBMenuItem',
-                'name' => 'apple',
-                'text' => 'Apple',
-                'URL' => '/apple/',
-            ];
-
-            $menuSpec->items = $items;
-        }
-
-        if ($menuSpec != $originalMenuSpec) {
-            CBDB::transaction(function () use ($menuSpec) {
-                CBModels::save($menuSpec);
-            });
-        }
+        CBModelUpdater::save(
+            $updater
+        );
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
     static function CBInstall_requiredClassNames(): array {
-        return ['YCMainMenu', 'CBViewPage'];
+        return [
+            'YCMainMenu',
+            'CBViewPage'
+        ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
+
+    /* -- functions -- */
+
+
 
     /**
-     * @return hex160
+     * @return CBID
      */
-    static function ID(): string {
+    static function ID(
+    ): string {
         return '95f30b62a9e5d761bbb9cb4c24e0dbc40493d134';
     }
+
 }
